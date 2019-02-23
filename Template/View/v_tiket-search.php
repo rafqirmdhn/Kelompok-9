3f1c7ce366a67486ca87efed452de15f132898d2
Zidanehanjar <zidanehanjar@gmail.com>
2019-02-23 12:45:09 +0700

    Edit view

--- Template/View/v_tiket-search.php
+++ Template/View/v_tiket-search.php
@@ -79,71 +79,7 @@
 					
 					
 					
-						<div class="judul">
-							<p class="judul-font"><b>Kenapa memilih JustTix?</b></p>
-						</div>
-					<div class="padding-left-600 margin-top-20">
 						
-						<br>
-						<!--Div pertama-->
-						<div>
-							
-							<div class="bagian-kiri">
-								<p class="bagian-font">Pembayaran Jujur</p>
-								Tidak terdapat biaya tambahan
-							</div>
-							<div class="bagian-kiri-text">
-								<img src="../Gambar/nofee.png" class="bagian-gambar" alt="Italian Trulli">
-							</div>
-							
-						
-						
-						</div>
-						<!--Div kedua-->
-						<div>
-							
-							<div class="bagian-kanan-text">
-								<p class="bagian-font">Banyak pilihan pembayaran</p>
-								Mempermudah User untuk membayar dengan banyak pilihan pembayaran
-							</div>
-							<div class="bagian-kanan">
-								<img src="../Gambar/payment1.png" class="bagian-gambar" alt="Italian Trulli">
-							</div>
-							
-						
-						</div>
-						
-						<!--Div ketiga-->
-						<div>
-							
-							<div class="bagian-kiri"">
-								<p class="bagian-font">Harga Murah Meriah</p>
-								Mendapat banyak pilihan dengan harga yang paling Terjangkau
-							</div>
-							<div class="bagian-kiri-text">
-								<img src="../Gambar/sales.png" class="bagian-gambar" alt="Italian Trulli">
-							</div>
-
-						</div>
-						
-						<!--Div Keempat-->
-						<div>
-							
-							<div class="bagian-kanan-text">
-								<p class="bagian-font">Agar Kelompok kami Lulus IMPAL</p>
-								Nilai bagus, Orang tua senang. menggapai cita cita yang mulia.
-							</div>
-							<div class="bagian-kanan"">
-								<img src="../Gambar/lulus1.png" class="bagian-gambar" alt="Italian Trulli">
-							</div>
-							
-						
-						</div>
-						
-						
-						
-						
-					</div>
 
             <!-- ... Your content goes here ... -->
 
