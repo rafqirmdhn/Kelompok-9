e16716f3a7223eebc285584a2cda454e600abd5e
IndrianaFadhila <dilladil05@gmail.com>
2019-02-23 11:28:29 +0700

    edit tiket

---
+++ Tiket/View/v_tiket.php
@@ -0,0 +1,129 @@
+<div id="page-wrapper" style="margin-left: 0"  >
+                <div class="container-fluid">
+                    <div class="headline">
+                        <div>JustTix</div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel panel-default">
+                                
+                                <!-- /.panel-heading -->
+                                <div class="panel-body">
+                                    
+                                    <div class="table-responsive" style=" overflow-x: hidden;">
+                                        <div class="headline" style="width:1369px; height: 250px">
+                                            <img src="../Gambar/pesawat.png" alt="" style="position: absolute; margin-left: -50px; margin-top: -50px; width:1369px;"> 
+                                            <h2 style="z-index: 0 margin-left:50px;position: absolute; text-align: right;left: 600px;top: 100px;color: white; font-size: 50px">JustTix</h2>
+
+                                        </div>
+                                    </div>
+                                    <div style="margin-top: 50px">
+                                        <p style="margin-left: 140px" style="font-size: 20px">Hasil Pencarian</p>
+                                    	<b style="margin-left: 140px" style="font-size: 20px"> Penerbangan Soekarno Hatta(Jakarta) - Ngurah Rai (Bali) </b>
+                                        <p style="margin-left: 140px">Minggu, 10 Februari </p>
+                                        <table id="tikettable" style="width:900px; overflow-x: hidden">
+                                            <thead>
+                                                <tr>
+                                                    <th>Pesawat</th>
+                                                    <th>Kelas Penerbangan</th>
+                                                    <th>Asal</th>
+                                                    <th>Tujuan</th>
+                                                    <th>Jam Terbang</th>
+                                                    <th>Jam Tiba</th>
+                                                    <th>Harga</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr class="odd gradeX">
+                                                    <td>Garuda Indonesia</td>
+                                                    <td>First Class</td>
+                                                    <td>Soekarno Hatta (Jakarta)</td>
+                                                    <td>Ngurah Rai (Bali)</td>
+                                                    <td>14.00</td>
+                                                    <td>16.00</td>
+                                                    <td>30.000.000
+                                                        <button type="button" class="btn-pil">Pilih</button>
+                                                    </td>
+                                                </tr>
+                                                <tr class="odd gradeX">
+                                                    <td>Lion Air</td>
+                                                    <td>Ekonomi</td>
+                                                    <td>Soekarno Hatta (Jakarta)</td>
+                                                    <td>Ngurah Rai (Bali)</td>
+                                                    <td>07.30</td>
+                                                    <td>09.00</td>
+                                                    <td>800.000
+                                                        <button type="button" class="btn-pil">Pilih</button>
+                                                    </td>
+                                                </tr>
+                                                <tr class="odd gradeX">
+                                                    <td>Garuda Indonesia</td>
+                                                    <td>Bisnis</td>
+                                                    <td>Soekarno Hatta (Jakarta)</td>
+                                                    <td>Ngurah Rai (Bali)</td>
+                                                    <td>19.00</td>
+                                                    <td>21.00</td>
+                                                    <td>10.000.000
+                                                        <button type="button" class="btn-pil">Pilih</button>
+                                                    </td>
+                                                </tr>
+                                                <tr class="odd gradeX">
+                                                    <td>Garuda Indonesia</td>
+                                                    <td>Ekonomi</td>
+                                                    <td>Soekarno Hatta (Jakarta)</td>
+                                                    <td>Ngurah Rai (Bali)</td>
+                                                    <td>15.30</td>
+                                                    <td>17.00</td>
+                                                    <td>1.500.000
+                                                        <button type="button" class="btn-pil">Pilih</button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <!-- /.table-responsive -->
+                                    
+                                </div>
+                                <!-- /.panel-body -->
+                            </div>
+                            <!-- /.panel -->
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- jQuery -->
+        <script src="../js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="../js/metisMenu.min.js"></script>
+
+        <!-- DataTables JavaScript -->
+        <script src="../js/dataTables/jquery.dataTables.min.js"></script>
+        <script src="../js/dataTables/dataTables.bootstrap.min.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="../js/startmin.js"></script>
+
+        <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+        <script>
+            $(document).ready(function() {
+                $('#dataTables-example').DataTable({
+                        responsive: true
+                });
+            });
+        </script>
+
+    </body>
+</html>
