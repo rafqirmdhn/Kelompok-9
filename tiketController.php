e82a1162622e0076bc39beff89b17ae5669abc50
rafqirmdhn <rafqiramadhan@gmail.com>
2019-02-26 22:45:11 +0700

    controller tiket

---
+++ tiketController.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * 
+	 */
+	class tiketController 
+	{
+		public function pembayaranP(){
+			include ("../Tiket/view/v_head.php");
+			include ("../Tiket/view/v_header.php");
+			include ("../Tiket/view/v_tiket.php");
+			include ("../Tiket/tiket.php")
+			}
+	}
+		function __construct(argument)
+		{
+			
+		}
+	}
+?>
