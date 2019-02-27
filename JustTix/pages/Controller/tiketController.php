a79e242192b88a5d392255542181ad0db1c8a7a4
IndrianaFadhila <dilladil05@gmail.com>
2019-02-27 11:26:52 +0700

    revisi 1 27/2/19

---
+++ JustTix/pages/Controller/tiketController.php
@@ -0,0 +1,45 @@
+<?php
+echo '<table id="tikettable" style="width:900px; overflow-x: hidden">';
+echo "<tr><th>No Penerbangan</th><th>Kelas</th><th>Asal</th><th>Tujuan</th><th>Harga</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "impal_test";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT no_penerbangan, kelas, asal, tujuan, harga FROM tiket WHERE asal = 'Soekarno Hatta (Jakarta)'"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
